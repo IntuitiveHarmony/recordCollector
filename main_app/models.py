30b2b71305905ac2b302d44c47ae38c43499b167
Intuitive Harmony <j.horst77@gmail.com>
2023-03-29 22:42:58 -0600

    Added Admin

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
-class Album(models.Model):
+class Record(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Record
 
-records = [
-    {'title': 'Cannity', 'artist': 'Jasper Cannity', 'release_year': 1983, 'image': 'https://cdn.midjourney.com/ea3b6ac2-ec61-4b15-9623-51030613dadd/0_3.png', 'track_list': ['Singin\' to the wind', 'Mary-Belle', 'The River Knows', 'Goin\' Downtown', 'Fields of Gold', 'Can\'t go on']},
-    {'title': 'Folk', 'artist': 'Sheppard Family Singers', 'release_year': 1958, 'image': 'https://cdn.midjourney.com/e1e9c890-c8f2-4a95-bb68-57dc9b7b6d0b/0_3.png', 'track_list': ['Mountain Man', 'Wanderlust', 'Winter\'s Chill', 'Homecoming', 'Fiddler\'s Green', 'Heartland']}
-]
+# records = [
+#     {'title': 'Cannity', 'artist': 'Jasper Cannity', 'release_year': 1983, 'image': 'https://cdn.midjourney.com/ea3b6ac2-ec61-4b15-9623-51030613dadd/0_3.png', 'track_list': ['Singin\' to the wind', 'Mary-Belle', 'The River Knows', 'Goin\' Downtown', 'Fields of Gold', 'Can\'t go on']},
+#     {'title': 'Folk', 'artist': 'Sheppard Family Singers', 'release_year': 1958, 'image': 'https://cdn.midjourney.com/e1e9c890-c8f2-4a95-bb68-57dc9b7b6d0b/0_3.png', 'track_list': ['Mountain Man', 'Wanderlust', 'Winter\'s Chill', 'Homecoming', 'Fiddler\'s Green', 'Heartland']}
+# ]
 
 # Create your views here.
 def home(request):
@@ -11,6 +12,7 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 def records_index(request):
+    records = Record.objects.all()
     return render(request, 'records/index.html', {
         'records': records
     })
