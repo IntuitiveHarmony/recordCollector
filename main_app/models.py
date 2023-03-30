a6b461737f4c00b23946cd10c22dd948537da96e
Intuitive Harmony <j.horst77@gmail.com>
2023-03-29 22:26:43 -0600

    Record model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    trackList = ArrayField(
+        models.CharField(max_length=50)
+    )
